e4efa74b4070dbb2b79b1b262082111daa202d75
Maxwell Gogo <96257757+Max02well@users.noreply.github.com>
2026-07-15 13:36:50 +0300

    Update ollama_client.py

--- src/llm/ollama_client.py
+++ src/llm/ollama_client.py
@@ -1,8 +1,11 @@
 from openai import OpenAI,APIConnectionError
+import os
 
 client = OpenAI(
-    base_url="http://localhost:11434/v1",
-    api_key="ollama"
+    # base_url="http://localhost:11434/v1",
+    # api_key="ollama"
+    api_key=os.getenv("LLM_API_KEY", ""),
+    base_url=os.getenv("LLM_BASE_URL", "http://localhost:11434/v1"),
 )
 
 def generate(prompt):
@@ -10,7 +13,8 @@ def generate(prompt):
         response = client.chat.completions.create(
 
             # model="llama3.1",
-            model="qwen2.5:7b",
+            # model="qwen2.5:7b",
+             model=os.getenv("LLM_MODEL"),
 
             temperature=0,
             # stream= True,
@@ -25,6 +29,6 @@ def generate(prompt):
 
         return response.choices[0].message.content
     except APIConnectionError:
-        return "Unable to connect to the Ollama server."
+        return "Unable to connect to the configured LLM provider."
     except Exception as e:
         return f"LLM Error: {e}"
